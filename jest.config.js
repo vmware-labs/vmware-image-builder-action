4b15282cabe00f3f0dc11191bad4013b1a8a6d19
Martin Perez <martinpe@vmware.com>
2022-11-04 08:23:23 +0100

    Make Jest like axios
    Signed-off-by: Martin Perez <martinpe@vmware.com>

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,16 @@
 module.exports = {
   clearMocks: true,
   moduleFileExtensions: ['js', 'ts'],
+  moduleNameMapper: {
+    "axios": "axios/dist/node/axios.cjs"
+  },
   roots: ['<rootDir>'],
   testEnvironment: 'node',
   testMatch: ['**/*.test.ts'],
   testRunner: 'jest-circus/runner',
+  testTimeout: 15000,
   transform: {
     '^.+\\.ts$': 'ts-jest'
   },
-  verbose: true,
-  testTimeout: 15000
+  verbose: true
 }
