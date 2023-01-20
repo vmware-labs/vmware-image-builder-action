29fa88d5110484bacbbb470a0f73f84f24b7a86e
Pablo Caraballo Llorente <pcaraballo@vmware.com>
2023-01-20 13:21:06 +0100

    Improve IT checks
    Signed-off-by: Pablo Caraballo Llorente <pcaraballo@vmware.com>

--- __tests__/src/action.it.test.ts
+++ __tests__/src/action.it.test.ts
@@ -2,6 +2,7 @@ import * as core from "@actions/core"
 import path from 'path'
 import Action from "../../src/action"
 import { TaskStatus } from '../../src/client/vib/api'
+import fs from 'fs'
 
 const TWO_MINUTES = 1200000
 
@@ -22,13 +23,14 @@ describe('Given a VIB Action', () => {
 
   it('When it is executed then it returns the final ActionResult', async () => {
     const result = await action.main()
-
-    expect(result.artifacts.length).toBe(9)
+    
     expect(result.executionGraph.status).toBe(TaskStatus.Succeeded)
     expect(result.executionGraph.tasks.length).toBe(4)
     expect(result.executionGraphReport).toBeDefined
     expect(result.executionGraphReport?.passed).toBe(false)
     expect(result.executionGraphReport?.actions.length).toBe(1)
+    expect(result.artifacts.length).toBe(9)
+    result.artifacts.forEach(a => expect(fs.existsSync(a)).toBeTruthy())
   }, TWO_MINUTES)
 
   it('When the execution graph times out then it fails', async () => {
