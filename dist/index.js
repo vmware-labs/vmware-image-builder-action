1dc2ebb7a10f845fad037cefc34a09db0670ad90
Gloria Delgado <gloriad@vmware.com>
2023-04-17 13:46:00 +0200

    Updating logic to show the CVE message
    Signed-off-by: Gloria Delgado <gloriad@vmware.com>

--- dist/index.js
+++ dist/index.js
@@ -396,6 +396,7 @@ class Action {
                     + `${ansi_colors_1.default.bold(ansi_colors_1.default.red(`${task.vulnerabilities.critical} critical`))}, `
                     + `${task["vulnerabilities"]["unknown"]} unknown`);
                 vulnerabilitiesTable += this.vulnerabilitiesTableRow(task.action_id, task.vulnerabilities.minimal, task.vulnerabilities.low, task.vulnerabilities.medium, task.vulnerabilities.high, task.vulnerabilities.critical, task.vulnerabilities.unknown, task.passed);
+                vulnerabilitiesTable += core.summary.addQuote("#x2139 The threshold is configured to fail only for packages type OS and CRITICAL vulnerabilities.");
             }
         }
         const tasksSkipped = executionGraph.tasks.filter(t => t.status === api_1.TaskStatus.Skipped).length;
@@ -411,7 +412,6 @@ class Action {
         const vulnerabilitiesTableRows = vulnerabilitiesTable.split("<tr>").length - 1;
         if (vulnerabilitiesTableRows > 2) {
             core.summary.addRaw(vulnerabilitiesTable);
-            core.summary.addQuote("#x2139The threshold is configured to fail only for packages type OS and CRITICAL vulnerabilities.");
         }
         if (process.env.GITHUB_STEP_SUMMARY)
             core.summary.write();
